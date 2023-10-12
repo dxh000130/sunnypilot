f200891dc4f5b7669ff1612abd1870dcf780105f
Xuheng Duan <34475380+dxh000130@users.noreply.github.com>
2023-10-13 10:34:27 +1300

    Update test.py

--- selfdrive/car/volkswagen/test.py
+++ selfdrive/car/volkswagen/test.py
@@ -7,11 +7,22 @@ PORT = 65432
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     data = ''
+
     while True:
         packet = s.recv(4096).decode('utf-8')
-        if not packet: 
+        
+        if not packet:
             break
+            
         data += packet
+        
+        # 尝试反序列化 JSON
+        try:
+            deserialized_data = json.loads(data)
+            print(deserialized_data)
+            # 清除缓冲区以便接收新的 JSON 数据
+            data = ''
+        except json.JSONDecodeError:
+            # 如果反序列化失败，则打印原始数据
+            print("Incomplete JSON:", data)
 
-    deserialized_data = json.loads(data)
-    print(deserialized_data)
