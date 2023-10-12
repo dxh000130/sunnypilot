46077af9cd9a0bb3040257dcae8d6e04b612eb38
Xuheng Duan <34475380+dxh000130@users.noreply.github.com>
2023-10-12 23:22:19 +1300

    Create test.py

---
+++ selfdrive/car/volkswagen/test.py
@@ -0,0 +1,13 @@
+import socket
+
+def start_client():
+    HOST = '127.0.0.1'
+    PORT = 65432
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        while True:
+            data = s.recv(1024)
+            print('Received:', data.decode())
+
+start_client()
