27a8ac8c4c85d8a515e35b8e10013971e9363eaf
Xuheng Duan <34475380+dxh000130@users.noreply.github.com>
2023-10-14 12:20:01 +1300

    Update test.py

--- selfdrive/car/volkswagen/test.py
+++ selfdrive/car/volkswagen/test.py
@@ -1,8 +1,14 @@
 import socket
 import json
 
+def log_and_print(message, log_file):
+    print(message)
+    with open(log_file, 'a') as file:
+        file.write(message + '\n')
+
 HOST = '127.0.0.1'
 PORT = 65432
+LOG_FILE = '/data/openpilot/selfdrive/car/volkswagen/output.log'  # 你可以将这个路径修改为你希望的日志文件位置
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
@@ -19,10 +25,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         # 尝试反序列化 JSON
         try:
             deserialized_data = json.loads(data)
-            print(deserialized_data)
+            log_and_print(str(deserialized_data), LOG_FILE)
             # 清除缓冲区以便接收新的 JSON 数据
             data = ''
         except json.JSONDecodeError:
             # 如果反序列化失败，则打印原始数据
-            print("Incomplete JSON:", data)
-
+            log_and_print("Incomplete JSON: " + data, LOG_FILE)
